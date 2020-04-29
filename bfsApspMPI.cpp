2e8fe750ffae7c1cdb563ca1d805f58c17ef7d4f
Jude Naveen Raj Ilango <judenaveenraj@gmail.com>
2020-04-28 21:50:29 -0400

    Added stats, added sbatch script

--- bfsApspMPI.cpp
+++ bfsApspMPI.cpp
@@ -5,13 +5,25 @@
 #include <numeric>
 #include <mpi.h>
 #include <math.h>
+#include <cstring>
+#include <getopt.h>
+
+using namespace std;
+
 
 int RANK;
 int PROCS;
+int THREADS;
 
 
+void get_file_name(string &file , int order, int degree){
+    file = "n";
+    file += to_string(order); 
+    file += "d"; 
+    file += to_string(degree);
+    file += ".random.edges"; 
+}
 
-using namespace std;
 
 void getAllChildren(vector<vector<int>> &g, vector<int> &frontier, vector<int> &children, int* distances, int k){
 	//printf("%d\n", frontier.size());
@@ -30,21 +42,21 @@ void getAllChildren(vector<vector<int>> &g, vector<int> &frontier, vector<int> &
 void getAllChildrenParallel(vector<vector<int>> &g, vector<int> &frontier, vector<int> &children, int* distances, int k){
 	int count = 0;
 	vector<int> local_children = vector<int>{};
-	#pragma omp parallel private(local_children) num_threads(10)
+	#pragma omp parallel private(local_children) num_threads(THREADS)
 	{
-	#pragma omp for nowait
-        for(int i=0; i<frontier.size(); i++){
-                int node = frontier[i];
-                for(int j=0; j<g[node].size(); j++){
-                        int n = g[node][j];
-                        if(distances[n] == -1){
-                                distances[n] = k;
-                                local_children.push_back(n);
-                        }
-                }
-        }
-	#pragma omp critical
-	move(local_children.begin(), local_children.end(), back_inserter(children));
+		#pragma omp for nowait
+		for(int i=0; i<frontier.size(); i++){
+				int node = frontier[i];
+				for(int j=0; j<g[node].size(); j++){
+						int n = g[node][j];
+						if(distances[n] == -1){
+								distances[n] = k;
+								local_children.push_back(n);
+						}
+				}
+		}
+		#pragma omp critical
+		move(local_children.begin(), local_children.end(), back_inserter(children));
 	}
 }
 
@@ -84,7 +96,7 @@ int* bfsParallel(vector<vector<int>> g, int source){
         return distances;
 }
 
-void serialBfsApsp(vector<vector<int>> g){
+pair<float, float> serialBfsApsp(vector<vector<int>> g){
 	int chunkSize = ceil(1.0*g.size()/PROCS);
 	int startSrc = chunkSize * RANK;
         int endSrc   = chunkSize * (RANK+1);
@@ -103,9 +115,9 @@ void serialBfsApsp(vector<vector<int>> g){
 		diameter[i] = *max_element(distances, distances + g.size());
 		distance[i] = accumulate(distances, distances + g.size(), 0.0) / g.size();
 	}
-	cout << "Proc:" << RANK << "  "<< "Max diameter: " << *max_element(diameter, diameter + currSize) << endl;
-	cout << "Proc:" << RANK << "  " << "Avg distance: " << (accumulate(distance, distance + currSize, 0.0) / currSize) << endl;
-		
+	double maxDia = *max_element(diameter, diameter + currSize);
+	double avgDist = accumulate(distance, distance + currSize, 0.0) / currSize;
+	return make_pair(maxDia, avgDist);
 }
 
 pair<float, float> parallelBfsApsp(vector<vector<int>> g){
@@ -120,9 +132,9 @@ pair<float, float> parallelBfsApsp(vector<vector<int>> g){
 	float* diameter = new float[currSize];
 	float* distance = new float[currSize];
 	
-	#pragma omp parallel for num_threads(10)
+	#pragma omp parallel for num_threads(THREADS)
 	for(int i=0; i<currSize; i++){
-                int* distances = bfsParallel(g, startSrc+i);
+		int* distances = bfsParallel(g, startSrc+i);
 		diameter[i] = *max_element(distances, distances + g.size());
 		distance[i] = accumulate(distances, distances + g.size(), 0.0) / g.size();
 	}
@@ -146,37 +158,123 @@ int main(int argc, char *argv[]) {
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &PROCS);
 	MPI_Comm_rank(MPI_COMM_WORLD, &RANK);
-	string fileName = "n8096d5.random.edges"; 
-	int order = 8096;
-	int degree = 5;
+	THREADS = 1;
+	
+	string fileName;
+	int order = -1;
+	int degree = -1;
+	int serial = false;
+	int parallelBfs = false;
+	int threads = 1;
+	int procs = 1;
+
+	while (1) {
+		static struct option long_options[] =
+			{
+				{"serial",   	no_argument,       &serial, 1},
+				{"parallelbfs",no_argument,       &parallelBfs, 1},
+				{"order",  	required_argument, 0, 'o'},
+				{"degree",  	required_argument, 0, 'd'},
+				{"threads",  	required_argument, 0, 't'},
+				{"procs",  	required_argument, 0, 'p'},
+				{0, 0, 0, 0}
+			};
+		/* getopt_long stores the option index here. */
+		int option_index = 0;
+
+		int c = getopt_long (argc, argv, "o:d:t:p:", long_options, &option_index);
+
+		/* Detect the end of the options. */
+		if (c == -1)
+			break;
+
+		switch (c)
+		{
+			case 0:
+			/* If this option set a flag, do nothing else now. */
+			if (long_options[option_index].flag != 0)
+				break;
+			break;
+
+			case 'o':
+			order = atoi(optarg);
+			break;
+
+			case 'd':
+			degree = atoi(optarg);
+			break;
+
+			case 't':
+			threads = atoi(optarg);
+			break;
+
+			case 'p':
+			procs = atoi(optarg);
+			break;
+
+			case '?':
+			/* getopt_long already printed an error message. */
+			break;
+
+			default:
+			abort ();
+		}
+	}
+
+	THREADS = threads;
+
+	
+	if(RANK == 0){
+		if(order == -1 || degree == -1){
+			printf("Order and degree is not set\n");
+			MPI_Finalize();
+			return 0;
+		}
+	}
+	
+    
 	vector<vector<int>> g(order, vector<int>(degree));
 	int gsize = order*degree;
+		
+	// If Serial Run	
+	if(serial && RANK == 0){
+		get_file_name(fileName, order, degree); 
+	    g = getAdjacencyListVector(fileName,order);
+		double tt = omp_get_wtime();
+		pair<float, float> res = serialBfsApsp(g);
+		double timeTaken = omp_get_wtime() - tt;
+		printf("Sequential: Order = %d\t Degree = %d\tMaxDia = %f\tAvgDist = %f\tTime = %f\n", order, degree, res.first, res.second, timeTaken);
+	}
+
+	
 	vector<int> newg;
 	if(RANK == 0){
-		g = getAdjacencyListVector(fileName,order);
 		for(auto && v : g){
- 			 newg.insert(newg.end(), v.begin(), v.end());
+			newg.insert(newg.end(), v.begin(), v.end());
 		}
 	}
 	newg.resize(gsize);
+
 	MPI_Bcast(&newg[0], gsize, MPI_INT, 0, MPI_COMM_WORLD);
 	if(RANK != 0){
 		for(int i=0; i<newg.size(); i++){
 			g[i/degree][i%degree] = newg[i];
 		}
 	}
-	double tt = omp_get_wtime();
-	//serialBfsApsp(g);
-	//printf("sequential-bfs-apsp = %fs\n", omp_get_wtime() - tt);
-
-	tt = MPI_Wtime();
+	
+	MPI_Barrier(MPI_COMM_WORLD); 
+	double tt2 = MPI_Wtime();
 	pair<float, float> res = parallelBfsApsp(g);
 	//printf("PROC:%d parallel-bfs-apsp = %fs\n", RANK, MPI_Wtime() - tt);
 	if(RANK == 0){
-		printf("Overall time = %fs\n", MPI_Wtime() - tt);
-		printf("GLOBAL MAXDIA = %f   AVGDIST = %f \n", res.first, res.second);
+		double timeTaken = MPI_Wtime() - tt2;
+		printf("Parallel: Procs= %d\tThreads = %d\tOrder = %d\t Degree = %d\tMaxDia = %f\tAvgDist = %f\tTime = %f\n", procs, threads, order, degree, res.first, res.second, timeTaken);
 	}
+
+	
 	MPI_Finalize();
 	return 0;
 	
 }
+
+
